7b9dced45ed458dcc80fa7424f350dcb306f3146
franciscolaranjeira <francisco.laranjeira@outlook.com>
2021-08-13 16:52:01 +0100

    começo layout info socios

---
+++ interface/info-socios.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="eng">
+    <head>
+        <title>Clube Nautico WEB</title>
+        <link rel="stylesheet" href="css/.css">
+    </head>
+    <body>
+        <div id = "screen">
+            <div id = "top-nav-bar">
+
+            </div>
+            <div id = "left-nav-bar">
+
+            </div>
+        </div>
+    </body>
+</html>
